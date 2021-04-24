3c7374faee400309e9846ba2f35c1d6bdd76e40f
cavslebron111-james <cavslebron111@gmail.com>
2021-04-24 18:36:15 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -2,48 +2,13 @@ import React, { Component } from 'react';
 import {Switch,Route} from "react-router-dom";
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from './components/Navbar';
-import Productlist from './components/Productlist';
-import Details from './components/Details';
-import Cart from './components/Cart';
-import Default from './components/Default';
-import Modal from './components/Modal';
-
-
-
 
 
 class App extends Component {
   render() {
     return (
+      <div className="app">hey it me app</div>
   
-  <React.Fragment>
-     <Navbar></Navbar>
-     <Switch>
-
-      <Route exact path="/" component={Productlist}/>
-      <Route path="/Details" component={Details}/>
-      
-      <Route path="/cart" component={Cart}/>
-      
-      <Route component={Default}/>
-     
-
-
-     </Switch>
-     < Modal />
-     
- 
- 
-
- 
-
-
-
-
-
-  </React.Fragment>
-    
 
     );
   }
